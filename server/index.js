4b477c4e8b6cc9c8592213ff610d4b7b60eb2fdc
krisnawiraska <krisnaswiraska17@gmail.com>
2023-10-05 13:22:51 +0800

    push route testing

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("testing");
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/data-catalog', (req, res) => {
+    res.json([
+        {
+            id : 1,
+            catalog_name : "push",
+        }
+    ])
+})
+  
+
+app.listen(port, () => {
+    console.log(`berhasil ${port}` )
+})
+
